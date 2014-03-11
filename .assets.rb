2a1689eef5b57434bc68b7d72ae8320c83d17f02
Joe Corcoran <joecorcoran@gmail.com>
2014-03-11 19:06:16 +0100

    Update Pannier config

--- .assets.rb
+++ .assets.rb
@@ -3,32 +3,32 @@ require 'yui/compressor'
 
 input  'assets'
 output 'public'
+logger
 
-class CssSquasher
-  def call(content, basename)
-    [yui.compress(content), basename]
-  end
-
-  def yui
-    @yui ||= YUI::CssCompressor.new
+behavior :compress do
+  modify do |content, _|
+    yui = YUI::CssCompressor.new
+    [yui.compress(content), _]
   end
 end
 
 package :sass do
   input  'sass'
   assets '**/*.scss'
+
   modify do |content, basename|
     [Sass.compile(content), basename]
   end
   concat 'main.min.css'
-  modify CssSquasher.new
+  behave :compress
 end
 
 package :fa do
   input  'fonts'
   assets '*.css'
+
   concat 'fa.min.css'
-  modify CssSquasher.new
+  behave :compress
 end
 
 package :fonts do
